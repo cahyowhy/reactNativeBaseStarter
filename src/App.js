b273cc3887dddec6ab4d75c9010e0c897c7cb210
cahyowhy <cahyowhy01@gmail.com>
2017-05-01 18:25:38 +0700

    ano

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+import HeaderNav from './Components/HeaderNav';
+import FooterFoot from './Components/FooterFoot';
+import Styles from './Components/Styles';
+import ListAvatarExample from './Components/ListAvatarExample';
+let timer;
+export default class anyarReact extends Component {
+    constructor(props){
+        super(props);
+        this.state={isScrolling:false, isDrawerOpened:false};
+        this.onScrolled=this.onScrolled.bind(this);
+        this.onDrawerOpened=this.onDrawerOpened.bind(this);
+    }
+    onDrawerOpened(){
+        const context = this;
+        this.setState({
+            isDrawerOpened: !context.state.isDrawerOpened
+        });
+    }
+    onScrolled(){
+        const context = this;
+        this.setState({
+            isScrolling: true
+        });
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            context.setState({isScrolling:false});
+        }, 1000);
+    }
+    render() {
+        return (
+            <View style={Styles.MainView}>
+                <HeaderNav isDrawerOpened={this.state.isDrawerOpened} onDrawerOpened={this.onDrawerOpened}/>
+                <ListAvatarExample onScrolled={this.onScrolled} isScrolling={this.state.isScrolling}/>
+                <FooterFoot hide={this.state.isScrolling || this.state.isDrawerOpened}/>
+            </View>
+        );
+    }
+}
--- src/Components/ListData.js
+++ src/Components/ListData.js
@@ -1,50 +1,60 @@
 const items = [
     {
+        id: 1,
         note: "reprehenderit nisi consectetur pariatur quis ea aliquip",
         name: "deserunt",
         registered: "03:32"
     },
     {
+        id: 2,
         note: "culpa cupidatat sit deserunt sint aute cillum",
         name: "tempor",
         registered: "08:42"
     },
     {
+        id: 3,
         note: "deserunt laboris reprehenderit occaecat aliquip cupidatat labore",
         name: "laborum",
         registered: "08:07"
     },
     {
+        id: 4,
         note: "sunt velit cillum cillum esse ipsum eiusmod",
         name: "sint",
         registered: "11:42"
     },
     {
+        id: 5,
         note: "nisi aliqua cillum commodo ullamco est exercitation",
         name: "nostrud",
         registered: "08:14"
     },
     {
+        id: 6,
         note: "anim non ad sunt eiusmod do pariatur",
         name: "enim",
         registered: "03:21"
     },
     {
+        id: 7,
         note: "in sunt cupidatat cupidatat magna est id",
         name: "do",
         registered: "01:21"
     },
     {
+        id: 8,
         note: "consequat tempor proident ea aliqua eu esse",
         name: "quis",
         registered: "09:20"
     },
     {
+        id: 9,
         note: "non deserunt anim sit culpa non officia",
         name: "et",
         registered: "12:08"
     },
     {
+        id: 10,
         note: "sunt ut eiusmod aute commodo officia proident",
         name: "Lorem",
         registered: "05:43"
