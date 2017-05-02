98cc987c0edcb7da937a173ca5114f33e19ce2d0
cahyowhy <cahyowhy01@gmail.com>
2017-05-02 16:17:00 +0700

    ano

--- src/Components/Styles.js
+++ src/Components/Styles.js
@@ -1,4 +1,5 @@
 import Dimensions from 'Dimensions';
+import {StyleSheet} from 'react-native';
 
 const width = Dimensions.get('window').width * 0.8;
 const height = Dimensions.get('window').height;
@@ -7,9 +8,19 @@ const Styles = {
         position: 'absolute', flex: 0.1, left: 0, right: 0, bottom: 0, backgroundColor: 'green',
         flexDirection: 'row', height: 50, alignItems: 'center'
     },
-    TextWhite:{color:'white'},
+    TextWhite: {color: 'white'},
     MainView: {flex: 1, flexDirection: 'column'},
     Hide: {display: 'none'},
+    mapContainer: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
     DrawerMain: {width: width, height: height, backgroundColor: 'white', position: 'absolute', zIndex: 21}
 };
 
--- src/Direction.js
+++ src/Direction.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import Style from './Components/Styles';
 
 export default class Direction extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            region:{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }
+        }
+    }
     render() {
         return (
-            <View><Text>Jajal</Text></View>
+            <View style={Style.mapContainer}>
+                <MapView style={Style.map} region={this.state.region}>
+                </MapView>
+            </View>
         )
     }
 }
