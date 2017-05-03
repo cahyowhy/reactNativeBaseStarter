14606b4c210b4d6e3cde99e53167521c805e552e
cahyowhy <cahyowhy01@gmail.com>
2017-05-03 10:56:41 +0700

    ano

---
+++ src/Components/AutoCompleteMap.js
@@ -0,0 +1,42 @@
+import RNGooglePlaces from 'react-native-google-places';
+import {TouchableOpacity, View, Text} from 'react-native';
+import {Button} from 'native-base';
+import React, {Component} from 'react';
+import Style from './Styles';
+export default class AutoCompleteMap extends Component {
+    constructor(props) {
+        super(props);
+        this.setNewPlaces = this.setNewPlaces.bind(this);
+    }
+
+    setNewPlaces(latlng) {
+        this.props.onSetNewPlace(latlng);
+    }
+
+    openSearchModal() {
+        const context = this;
+        RNGooglePlaces.openAutocompleteModal()
+            .then((place) => {
+                console.log(place);
+                let latlng = {
+                    latitude: place.latitude,
+                    longitude: place.longitude
+                };
+
+                context.setNewPlaces(latlng);
+                // place represents user's selection from the
+                // suggestions and it is a simplified Google Place object.
+            })
+            .catch(error => console.log(error.message));  // error is a Javascript Error object
+    }
+
+    render() {
+        return (
+            <View>
+                <Button style={Style.ButtonPickedPlace} onPress={() => this.openSearchModal()}>
+                    <Text style={Style.TextButtonPickedPlace}>Pick a Place</Text>
+                </Button>
+            </View>
+        );
+    }
+}
--- src/Direction.js
+++ src/Direction.js
@@ -2,24 +2,81 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 import Style from './Components/Styles';
+import {Button, Icon} from 'native-base';
+import AutoCompleteMap from './Components/AutoCompleteMap';
 
 export default class Direction extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            region:{
+        this.state = {
+            region: {
                 latitude: 37.78825,
                 longitude: -122.4324,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
+            },
+            marker: {
+                latlng: {
+                    latitude: 37.78825,
+                    longitude: -122.4324
+                },
+                title: 'my marker',
+                description: 'some description ...'
             }
-        }
+        };
+        this.onRegionChange = this.onRegionChange.bind(this);
+        this.setMarkerPosition = this.setMarkerPosition.bind(this);
     }
+
+    onRegionChange(region) {
+        this.setState({region});
+        /*console.log(region.latitude);
+         this.setState({
+         marker: {
+         latlng: {
+         latitude: region.latitude,
+         longitude: region.longitude
+         }
+         }
+         });*/
+    }
+
+    static navigationOptions = ({navigation}) => ({
+        title: 'MapView',
+        headerRight: <Button transparent onPress={()=>alert("hay")}><Icon name="menu"/></Button>
+    });
+
+    setMarkerPosition(latlng) {
+        this.setState({
+            marker: {
+                latlng: {
+                    latitude: latlng.latitude,
+                    longitude: latlng.longitude
+                }
+            }
+        });
+        this.setState({
+            region: {
+                latitude: latlng.latitude,
+                longitude: latlng.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }
+        });
+    }
+
     render() {
         return (
             <View style={Style.mapContainer}>
-                <MapView style={Style.map} region={this.state.region}>
+                <MapView style={Style.map} region={this.state.region} onRegionChange={this.onRegionChange}>
+                    <MapView.Marker draggable
+                                    coordinate={this.state.marker.latlng}
+                                    title={this.state.marker.title}
+                                    description={this.state.marker.description}
+                                    onDrag={() => console.log('onDrag')}
+                    />
                 </MapView>
+                <AutoCompleteMap onSetNewPlace={this.setMarkerPosition}/>
             </View>
         )
     }
