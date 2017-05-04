8c3aaa008d752f67e0dae9cfa0bcf794dafd57c2
cahyowhy <cahyowhy01@gmail.com>
2017-05-04 14:21:07 +0700

    adding alt

--- src/Camera.js
+++ src/Camera.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import LocationStore from './Components/AltFluxTutor/Store/LocationStore';
+import LocationActions from  './Components/AltFluxTutor/Actions/LocationActions';
 
 export default class Camera extends Component {
+    constructor(props) {
+        super(props);
+        this.state = LocationStore.getState();
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidMount() {
+        LocationStore.listen(this.onChange);
+        LocationActions.fetchLocations();
+    }
+
+    componentWillUnmount() {
+        LocationStore.unlisten(this.onChange);
+    }
+
+    onChange(state) {
+        this.setState(state);
+    }
+
     render() {
+        if (this.state.errorMessage) {
+            return (
+                <View>Something is wrong</View>
+            );
+        }
+
+        if (!this.state.locations.length) {
+            return (
+                <View>
+                    <Image style={{width: 50, height: 50}} source={{require: './img/loading.gif'}}/>
+                </View>
+            )
+        }
+
         return (
-            <View><Text>Jajal</Text></View>
-        )
+            <View>
+                {this.state.locations.map((location) => {
+                    return (
+                        <Text>{location.name}</Text>
+                    );
+                })}
+            </View>
+        );
     }
 }
---
+++ src/Components/AltFluxTutor/Alt.js
@@ -0,0 +1,3 @@
+import Alt from 'alt';
+
+export const alt = new Alt();
