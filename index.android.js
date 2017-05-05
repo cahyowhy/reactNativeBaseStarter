e0b4250782c5b720a3d7bf8f96d28daa15a8bb5d
cahyowhy <cahyowhy01@gmail.com>
2017-05-05 13:33:21 +0700

    adf

--- index.android.js
+++ index.android.js
@@ -8,11 +8,11 @@ import React, {Component} from 'react';
 import { AppRegistry } from 'react-native';
 /*import App from './src/App';*/
 /*import {BasicApp} from './src/example/BasicApp';*/
-import {Route} from './src/Router';
+import {Tab} from './src/Router';
 export default class anyarReact extends Component {
     render() {
         return (
-            <Route/>
+            <Tab/>
         );
     }
 }
--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,38 @@
 import {
-    StackNavigator,
+    StackNavigator, TabNavigator
 } from 'react-navigation';
 import Profile from './Profile';
 import Camera from './Camera';
 import Direction from './Direction';
 import People from './People';
 import Main from './App';
+import {Icon} from 'native-base';
+import React, {Component} from 'react';
 
-export const Route = StackNavigator({
+export const Tab = TabNavigator({
+    Main: {
+        screen: Main,
+        tabBarIcon: <Icon name="apps" />
+    },
+    Camera: {
+        screen: Camera,
+        tabBarIcon: <Icon name="apps" />
+    },
+    Direction: {
+        screen: Direction,
+        tabBarIcon: <Icon name="camera" />
+    },
+    /*Profile: {
+     screen: Profile,
+     tabBarIcon: <Icon name="navigate" />,
+     },*/
+    People: {
+        screen: People,
+        tabBarIcon: <Icon name="person" />,
+    }
+});
+
+const Route = StackNavigator({
     Main: {
         screen: Main,
         navigationOptions: {
