a53c2898da8c3e724eaf88ce499fb6c4a0ef22ff
cahyowhy <cahyowhy01@gmail.com>
2017-05-08 15:19:44 +0700

    git push

--- jajalMap/FeedDetailView.js
+++ jajalMap/FeedDetailView.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 import Style from './Style';
+import Utils from './RealmDb/Utils';
 
-export default class Profile extends Component {
+export default class FeedDetailView extends Component {
     watchID: ?number = null;
 
     constructor(props) {
@@ -21,28 +22,32 @@ export default class Profile extends Component {
                     latitude: 0.0,
                     longitude: 0.0
                 }
-            }
+            },
+            polylines: []
         };
     }
 
     setPosition(position) {
-        this.setState({
-            marker: {
-                latlng: {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
-                }
-            }
-        });
-
         this.setState({
             region: {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
-            }
+            },
+            marker: {
+                latlng: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                }
+            },
+            polylines: this.state.polylines.concat([{
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }])
         });
+
+        console.log(this.state.polylines);
     }
 
     componentDidMount() {
@@ -68,6 +73,15 @@ export default class Profile extends Component {
             <View style={Style.mapContainer}>
                 <MapView style={Style.map} region={this.state.region}>
                     <MapView.Marker coordinate={this.state.marker.latlng}/>
+                    {this.state.polylines.map(polyline => (
+                        <MapView.Polyline
+                            key={Utils.guid()}
+                            coordinates={this.state.polylines}
+                            strokeColor="#000"
+                            fillColor="rgba(255,0,0,0.5)"
+                            strokeWidth={1}
+                        />
+                    ))}
                 </MapView>
             </View>
         )
